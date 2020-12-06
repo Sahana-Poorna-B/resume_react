6e9c1c8bfc3363ad759881fcfbd893ce0d803674
Sahana-Poorna-B <75526427+Sahana-Poorna-B@users.noreply.github.com>
2020-12-06 19:24:59 +0530

    Add files via upload

---
+++ src/components/about/About.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { motion } from 'framer-motion'
+import './about.css'
+
+const About = () => {
+    const about_variants = {
+        hidden: {
+            opacity: 0
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                delay: 0.2, duration: 0.6,
+            }
+        },
+        exit: {
+            opacity: 0,
+            transition: {
+                ease: 'easeInOut'
+            }
+        }
+    }
+    return (
+        <motion.div className="about"
+
+            variants={about_variants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+        >
+            <h6 className="about__intro">
+                Enthusiastic and motivated student pursuing B.Tech in Computer Engineering at Presidency University.
+            </h6>
+            
+        </motion.div>
+    );
+};
+
+export default About;
---
+++ src/components/data/resume_data.js
@@ -0,0 +1,28 @@
+import react from '../../assets/icons/react.svg'
+import python from '../../assets/icons/python.svg'
+
+
+const languages = [
+    {
+        icon: python,
+        name: 'Python',
+        level: '75'
+    },
+    {
+        icon: react,
+        name: 'C',
+        level: '50'
+    },
+    {
+        icon: react,
+        name: 'HTML',
+        level: '65'
+    },
+    {
+        icon: react,
+        name: 'CSS',
+        level: '50'
+    }
+]
+
+export {languages }
